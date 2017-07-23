e8ae90982893fe7e4b8c026983c5dd35117238c8
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-07-23 17:43:58 +0900

    😢

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
-  var $heart = $(".heart");
+  var $heart = $(".heart"),
+    $ellipsis = $(".fa-ellipsis-h"),
+    $overlay = $(".overlay");
   $heart.click(function() {
-    $(this).toggleClass("fa-heart-o 8");
+    $(this).toggleClass("fa-heart-o heart-pumping fa-heart");
   });
+
+  $ellipsis.click(function() {
+    $overlay.fadeIn(1000);
+  });
+
+  //fadeOut
 });
