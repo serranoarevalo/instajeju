f48c47084fdefe32cf0e0268621f9bbba4d762e0
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-07-23 16:58:18 +0900

    💀

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-alert("lalalal");
+$(document).ready(function() {
+  var $heart = $(".heart");
+  $heart.click(function() {
+    if ($(this).hasClass("heart-pumping")) {
+      $(this).removeClass("heart-pumping");
+    } else {
+      $(this).addClass("heart-pumping");
+    }
+  });
+});
